d1b5f50afc3d30ac1d863f97143c89a97ae81138
RabbitMin <115143560+RabbitMin@users.noreply.github.com>
2023-01-17 02:23:39 +0900

    Add files via upload

---
+++ Exercise 1.py
@@ -0,0 +1,20 @@
+# Exercise 1: Calculate the multiplication and sum of two numbers
+
+#Given 1
+
+number1 = 20
+number2 = 30
+
+multi = number1 * number2
+
+print("The result is " + str(multi))
+
+
+#Given 2
+
+number1 = 40
+number2 = 30
+
+add = number1 + number2
+
+print("The result is " + str(add))
