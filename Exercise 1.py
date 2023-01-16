34257277e47266a6b358fa60cf7e7bcdf19871ff
RabbitMin <115143560+RabbitMin@users.noreply.github.com>
2023-01-17 02:26:07 +0900

    Add files via upload

--- Exercise 1.py
+++ Exercise 1.py
@@ -6,15 +6,22 @@ number1 = 20
 number2 = 30
 
 multi = number1 * number2
+add = number1 + number2
 
-print("The result is " + str(multi))
-
+if multi <= 1000:
+    print("The result is " + str(multi))
+else:
+    print("The result is " + str(add))
 
 #Given 2
 
 number1 = 40
 number2 = 30
 
+multi = number1 * number2
 add = number1 + number2
 
-print("The result is " + str(add))
+if multi <= 1000:
+    print("The result is " + str(multi))
+else:
+    print("The result is " + str(add))
